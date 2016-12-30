b2d6ed683ef36e97991710d7967248c65dd33392
szymonwlochowski <szymonwlochowski@gmail.com>
2016-12-30 23:17:34 +0000

    Add products to seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'admin_sradmin@example.com', password: 'password', password_confirmation: 'password')
+
+niceWitcher = Product.create(title: "Geralt",
+ subtitle: "Best witcher", image_name: "witcher-thumb.jpg",
+ price: "4.99", sku: "witcherOne", download_url: "https://s3-us-west-2.amazonaws.com/stripe-app-bucket/witcher-thumb.jpg",
+ details: "A really nice witcher", description: %{<p>It's Geralt</p>
+ })
+
+niceCiri = Product.create(title: "Ciri",
+ subtitle: "Best witcher girl", image_name: "ciri.jpg",
+ price: "4.99", sku: "witcherTwo", download_url: "https://s3-us-west-2.amazonaws.com/stripe-app-bucket/ciri.jpg",
+ details: "A really nice witcher too", description: %{<p>It's Ciri</p>
+ })
